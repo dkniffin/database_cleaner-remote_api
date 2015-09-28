198eecbd1d4c5ec4726c526f447f7ee59b53f6ff
dkniffin <derek.kniffin@gmail.com>
2015-09-28 14:48:28 -0400

    Added an endpoint for starting the cleaner

--- app/controllers/database_cleaner/remote_api/database_cleans_controller.rb
+++ app/controllers/database_cleaner/remote_api/database_cleans_controller.rb
@@ -2,11 +2,15 @@ module DatabaseCleaner
   module RemoteApi
     class DatabaseCleansController < DatabaseCleaner::RemoteApi::ApplicationController
 
-      def show
+      def clean
         DatabaseCleaner.clean
         render json: {}
       end
 
+      def start
+        DatabaseCleaner.start
+        render json: {}
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 DatabaseCleaner::RemoteApi::Engine.routes.draw do
-  match 'clean' => 'database_cleans#show', via: [:get, :post], defaults: { format: :json }
+  match 'clean' => 'database_cleans#clean', via: [:get, :post], defaults: { format: :json }
+  match 'start' => 'database_cleans#start', via: [:get, :post], defaults: { format: :json }
 end
